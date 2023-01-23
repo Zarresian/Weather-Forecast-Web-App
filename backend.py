4dd1dff107d9bb039ca570b69d953e6a11f78cb5
Zarresian <93849777+Zarresian@users.noreply.github.com>
2023-01-23 16:34:19 +0000

    Add files via upload

---
+++ backend.py
@@ -0,0 +1,17 @@
+import requests
+
+API_KEY = "85881ac66b1ccdbdc30b88481349e6b0"
+
+
+def get_data(place, forecast_days=None):
+    url = f"https://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
+    response = requests.get(url)
+    data = response.json()
+    filtered_data = data["list"]
+    nr_values = 8 * forecast_days
+    filtered_data = filtered_data[:nr_values]
+    return filtered_data
+
+
+if __name__=="__main__":
+    print(get_data(place="Tokyo", forecast_days=3))
